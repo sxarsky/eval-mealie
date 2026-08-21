6c6fd887aafa9743fd5949958c137bebde27d45c
Skyramp Eval <eval@skyramp.dev>
2026-08-21 13:35:14 -0700

    feat: add categories_only query parameter to recipe list

--- mealie/routes/recipe/__init__.py
+++ mealie/routes/recipe/__init__.py
@@ -1,12 +1,13 @@
 from fastapi import APIRouter
 
-from . import bulk_actions, comments, exports, recipe_crud_routes, shared_routes, timeline_events
+from . import bulk_actions, comments, exports, recipe_crud_routes, recipe_list_routes, shared_routes, timeline_events
 
 prefix = "/recipes"
 
 router = APIRouter()
 
 router.include_router(exports.router, tags=["Recipe: Exports"])
+router.include_router(recipe_list_routes.router, tags=["Recipe: CRUD"])
 router.include_router(recipe_crud_routes.router, tags=["Recipe: CRUD"])
 router.include_router(comments.router, prefix=prefix, tags=["Recipe: Comments"])
 router.include_router(bulk_actions.router, prefix=prefix, tags=["Recipe: Bulk Actions"])
